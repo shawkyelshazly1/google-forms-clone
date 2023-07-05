fe581f40acf5134e4a73a73add7d342a821c4b0e
Shawky Elshazly <Shawkyelshazly2@gmail.com>
2023-07-05 20:38:23 +0300

    adjusted api url for production

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL:
 		process.env.REACT_APP_NODE_ENV === "development"
 			? "http://localhost:5000"
-			: "http://localhost:5000",
+			: "https://docs4u-api.onrender.com/",
 	headers: {
 		Accept: "application/json",
 		"Content-Type": "application/json; charset=UTF-8",
